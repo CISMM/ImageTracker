2ce17a2ec1c717f2228610c7b08f9113a1daa77d
beastwoo <beastwoo>
2007-02-11 23:35:18 +0000

    Added ApplyTransformsPipeline, which applies a set of transforms to a set of files.  Helpful for aligning images whose registration info comes from a different color channel.

---
+++ pipelines/ApplyTransformsPipeline.cxx
@@ -0,0 +1,82 @@
+#include "ApplyTransformsPipeline.h"
+
+#include <vector>
+
+#include "itkCastImageFilter.h"
+#include "itkResampleImageFilter.h"
+#include "itkCenteredRigid2DTransform.h"
+
+#include "ImageUtils.h"
+#include "Logger.h"
+#include "MultiResolutionRegistrationPipeline.h"
+#include "TransformGroup.h"
+
+void ApplyTransformsPipeline::Update()
+{
+    std::string function("ApplyTransformsPipeline::Update");
+    Logger::verbose << function << ": Checking parameters" << std::endl;
+    if (this->input == NULL ||
+        this->input->size() < 1 ||
+        this->outputFiles.size() < 1 ||
+        this->m_TransformFile == "")
+    {
+        Logger::warning << function << ": input and output not properly configured; aborting" << std::endl;
+        return;
+    }
+    
+    typedef MultiResolutionRegistrationPipeline::TransformType TransformType;
+    typedef std::vector< TransformType::Pointer > TransformVector;
+    
+    Logger::verbose << function << ": Loading the transforms from transform file" << std::endl;
+    TransformVector *pTransforms = TransformGroup::LoadTransforms(this->m_TransformFile);
+    if (pTransforms == NULL ||
+        pTransforms->size() < 1)
+    {
+        Logger::warning << function << ": tranform file " << this->m_TransformFile << " contains no transforms or is poorly formatted; aborting" << std::endl;
+        return;
+    }
+    
+    // Declare transform pipeline classes
+    typedef itk::ResampleImageFilter< InternalImageType, InternalImageType > ResampleType;
+    typedef itk::CastImageFilter< InternalImageType, WriteImageType > CastType;
+    
+    Logger::verbose << function << ": Setting up transform classes" << std::endl;
+    ResampleType::Pointer resample = ResampleType::New();
+    CastType::Pointer cast = CastType::New();
+    cast->SetInput(resample->GetOutput());
+    
+    Logger::verbose << function << ": Using first image to set resampling parameters" << std::endl;
+    InternalImageType::Pointer image(dynamic_cast< InternalImageType* >(this->input->GetImage(0)));
+    resample->SetSize(image->GetLargestPossibleRegion().GetSize());
+    resample->SetOutputOrigin(image->GetOrigin());
+    resample->SetOutputSpacing(image->GetSpacing());
+    resample->SetDefaultPixelValue(0);
+    
+    Logger::verbose << function << ": Creating base identity transform" << std::endl;
+    TransformType::Pointer transform = TransformType::New();
+    transform->SetIdentity();
+    
+    // Iterate through all transforms, making sure not to overrun either our input images or
+    // output file names
+    int index = 0;
+    for (TransformVector::iterator vecIt = pTransforms->begin();
+         vecIt != pTransforms->end() && 
+                 index < this->input->size() && 
+                 index < this->outputFiles.size();
+         ++vecIt, 
+         index++)
+    {
+        // Precompose the current transform with the previous one
+        transform->Compose(*vecIt, true);
+        
+        // Set the resample inputs
+        resample->SetInput(dynamic_cast< InternalImageType* >(this->input->GetImage(index)));
+        resample->SetTransform(transform);
+        
+        // Write the image
+        WriteImage(cast->GetOutput(), this->outputFiles[index]);
+    }
+    
+    Logger::verbose << function << ": Cleaning up dumb pointers" << std::endl;
+    delete (pTransforms);
+}
---
+++ pipelines/ApplyTransformsPipeline.h
@@ -0,0 +1,55 @@
+#pragma once
+
+#include <string>
+
+#include "itkImage.h"
+#include "itkObject.h"
+
+#include "CommonTypes.h"
+#include "FileSet.h"
+#include "ImageSetReader.h"
+
+/**
+ * \class ApplyTransformsPipeline
+ * \brief Applies a set of rigid transforms to a set of images.
+ * Given a set of files and a file containing a list of transforms, this
+ * pipeline applies the set of transforms to the set of images.  The transforms
+ * are composed, so each applied transform is a composition of all previous
+ * transforms.
+ */
+class ApplyTransformsPipeline :
+    public itk::Object
+{
+public:
+    // Standard itk typedefs
+    typedef ApplyTransformsPipeline Self;
+    typedef itk::Object Superclass;
+    typedef itk::SmartPointer< Self > Pointer;
+    typedef itk::SmartPointer< const Self > ConstPointer;
+    itkNewMacro(Self);
+    
+    itkGetMacro(TransformFile, std::string);
+    itkSetMacro(TransformFile, std::string);
+    
+    void SetInput(ImageSetReaderBase* input) { this->input = input; }
+    const FileSet& GetOutputFiles() { return this->outputFiles; }
+    void SetOutputFiles(const FileSet& files) { this->outputFiles = files; }
+
+    void Update();
+    
+    typedef CommonTypes::InputImageType WriteImageType;
+    typedef CommonTypes::InternalImageType InternalImageType;
+    
+protected:
+    ApplyTransformsPipeline() {}
+    virtual ~ApplyTransformsPipeline() {}
+    
+private:
+    // not implemented
+    ApplyTransformsPipeline(const Self& other);
+    void operator=(const Self& other);
+    
+    ImageSetReaderBase* input;
+    FileSet outputFiles;
+    std::string m_TransformFile;
+};
