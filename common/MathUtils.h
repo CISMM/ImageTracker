d6481f86fa2b7f816dde21e8770853bba5445d06
beastwoo <beastwoo>
2007-05-17 18:38:37 +0000

    VS8 was complaining this file was in MAC format.  I don't own a MAC, but I'm going along with it because converting it lets my project compile.

--- common/MathUtils.h
+++ common/MathUtils.h
@@ -1,22 +1,44 @@
 
+
 #include <cmath>
 
+
+
 #ifndef _MATHUTILS_H
+
 #define _MATHUTILS_H
 
+
+
 // Defines some helpful math functions and constants left out of
+
 // the Visual Studio math libraries.  I guess GNU's gcc adds a
+
 // little special sauce for math folks.
+
 #ifdef _WIN32
 
+
+
 // A definition of pi.
+
 #define M_PI 2*acos(0.0)
 
+
+
 // logs and exponents
+
 inline double log2(const double& num) { return (log10(num) / log10(2.0)); }
+
 inline double exp2(const double& num) { return pow(2.0, num); }
+
 inline double exp10(const double& num) { return pow(10.0, num); }
+
+
 
 #endif // ifdef _WIN32
 
+
+
 #endif // ifndef _MATHUTILS_H
+
