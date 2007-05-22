66b0becbb9fd1e8d9e9966fc309d146a7038f6e9
beastwoo <beastwoo>
2007-05-22 23:40:53 +0000

    Uses a wxProgressDialog to report itk pipeline execution progress.

---
+++ wxGui/WxPipelineObserver.cxx
@@ -0,0 +1,30 @@
+#include "WxPipelineObserver.h"
+
+#include "Logger.h"
+#include "wxUtils.h"
+
+bool WxPipelineObserver::Update(double progress, const std::string& message)
+{
+    bool notAbort = true;
+    if (this->dlgProgress)
+    {
+        int prog = (int) (progress * 100);
+        notAbort = this->dlgProgress->Update(prog, std2wx(message));
+        Logger::verbose << "WxPipelineObserver::Update: " << prog << ", " << message << ", " << 
+                (notAbort ? "continuing" : "aborting") << std::endl;
+    }
+    return !notAbort;
+}
+
+void WxPipelineObserver::CreateProgressDialog(const std::string& title, const std::string& message, int maximum, 
+                                              wxWindow* parent, int style)
+{
+    if (this->dlgProgress)
+    {
+        this->dlgProgress->Destroy();
+        this->dlgProgress = NULL;
+    }
+    
+    this->dlgProgress = new wxProgressDialog(std2wx(title), std2wx(message), maximum, parent, style);
+    this->dlgProgress->Show(true);
+}
---
+++ wxGui/WxPipelineObserver.h
@@ -0,0 +1,53 @@
+#pragma once
+
+#include <string>
+
+#include <wx/wx.h>
+#include <wx/progdlg.h>
+
+#include "itkObjectFactory.h"
+#include "ItkPipelineObserver.h"
+
+/**
+ * \class WxPipelineObserver
+ * \brief wxWidgets-based itk pipeline progress observer.
+ *
+ * WxPipelineObserver controls a wxProgressDialog that updates based on ItkPipeline
+ * progress events.
+ */
+class WxPipelineObserver : 
+    public ItkPipelineObserver
+{
+public:
+    typedef WxPipelineObserver Self;
+    typedef ItkPipelineObserver Superclass;
+    typedef itk::SmartPointer< Self > Pointer;
+    typedef itk::SmartPointer< const Self > ConstPointer;
+    itkNewMacro(Self);
+    itkTypeMacro(WxPipelineObserver, ItkPipelineObserver);
+    
+    virtual bool Update(double progress, const std::string& message);
+    
+    void CreateProgressDialog(const std::string& title = "Pipeline Progress", const std::string& message = "", int maximum = 100, 
+                              wxWindow* parent = NULL, int style = wxPD_APP_MODAL | wxPD_CAN_ABORT | wxPD_REMAINING_TIME | wxPD_SMOOTH);
+    
+protected:
+    WxPipelineObserver() 
+        : dlgProgress(0)
+    {
+        this->CreateProgressDialog();
+    }
+    
+    virtual ~WxPipelineObserver() 
+    {
+        if (this->dlgProgress)
+            this->dlgProgress->Destroy();
+    }
+    
+private:
+    // not implemented
+    WxPipelineObserver(const Self& other);
+    void operator=(const Self& other);
+    
+    wxProgressDialog* dlgProgress;
+};
