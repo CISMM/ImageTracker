add693422746dd98a92cd120c56bd5d797868c1b
beastwoo <beastwoo>
2007-06-21 18:19:14 +0000

    dispimg Displays and image in grayscale and scales it appropriately.

---
+++ matlab/dispimg.m
@@ -0,0 +1,18 @@
+function [] = dispimg(img, cmap)
+% dispimg(img, color) Display an image and scale appropriately.
+%
+% Display the given image with the chosen color map.
+% Inputs (default):
+% img      - The input image
+% cmap     - The colormap to use (gray)
+%
+% Outputs:
+% none
+
+if (nargin < 2)
+    cmap = 'gray';
+end;
+
+imagesc(img);
+axis image;
+colormap(cmap);
