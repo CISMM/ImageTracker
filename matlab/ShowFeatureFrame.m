75e2c5d512807977cf58aad715edf49b024dffbb
beastwoo <beastwoo>
2007-07-05 16:38:43 +0000

    Changed single frame display in the strain GUI to color-code the features in the same manner used by the feature movie display.

--- matlab/ShowFeatureFrame.m
+++ matlab/ShowFeatureFrame.m
@@ -4,9 +4,33 @@ function [ ] = ShowFeatureFrame( imgs, feats, index )
 
 idx = max(1,index);
 idx = min(idx, size(imgs,3));
+
+[hi,wi,ti] = size(imgs);
+[hf,wf,tf] = size(feats);
+ff = 1:hf;
+
+% show image
 dispimg(imgs(:,:,idx));
 
-fidx = find(feats(:,4,idx));
 hold on;
-plot(feats(fidx,2,idx), feats(fidx,1,idx), 'ys');
-hold off;
+if (idx == 1)    
+    % Show first frame
+    fidx = find(feats(:,4,idx));
+    plot(feats(fidx,2,1), feats(fidx,1,1), 'gs');
+else
+    % Show other frames
+    % Determine feature classes
+    newFeat(ff)  = (feats(ff,4,idx) == 1).*(feats(ff,4,idx-1)==0);
+    contFeat(ff) = (feats(ff,4,idx) == 1).*(feats(ff,4,idx-1)==1);
+    deadFeat(ff) = (feats(ff,4,idx) == 0).*(feats(ff,4,idx-1)==1);
+    newIdx  = find(newFeat);
+    contIdx = find(contFeat);
+    deadIdx = find(deadFeat);
+    
+    % display the features, color coded
+    plot(feats(newIdx, 2,idx), feats( newIdx,1,idx), 'gs');
+    plot(feats(contIdx,2,idx), feats(contIdx,1,idx), 'ys');
+    plot(feats(deadIdx,2,idx), feats(deadIdx,1,idx), 'rs');
+end;
+
+hold off; % done drawing on the figure
