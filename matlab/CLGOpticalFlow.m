4241a9bd40299cc13e556b469d90c74784e5471b
beastwoo <beastwoo>
2007-11-19 22:21:08 +0000

    Added check for change in size of computed flow at each step, as the error may not decrease at each step. De-vectorizing some code seemed to fix a convergence problem, but runs much slower.

--- matlab/CLGOpticalFlow.m
+++ matlab/CLGOpticalFlow.m
@@ -58,10 +58,21 @@ yint = 20:h-20;
 [xxg, yyg] = meshgrid(1:w,1:h);
 meanval = mean(img1(:));
 
+lastU = u;
+lastV = v;
+
 for i = 1:iterations
     for y = 2:h-1
-        u(y,xx) = (1-relax)*u(y,xx) + relax * (u(y-1,xx) + u(y+1,xx) + u(y,xx-1) + u(y,xx+1) - (Txy(y,xx).*v(y,xx) + Txt(y,xx)))/(4 + Txx(y,xx));
-        v(y,xx) = (1-relax)*v(y,xx) + relax * (v(y-1,xx) + v(y+1,xx) + v(y,xx-1) + v(y,xx+1) - (Txy(y,xx).*u(y,xx) + Tyt(y,xx)))/(4 + Tyy(y,xx));
+        for x = 2:w-1
+            sumU = u(y-1,x) + u(y+1,x) + u(y,x-1) + u(y,x+1);
+            sumV = v(y-1,x) + v(y+1,x) + v(y,x-1) + v(y,x+1);
+            u(y,x) = (1-relax)*u(y,x) + relax * (sumU - (Txy(y,x).*v(y,x) + Txt(y,x)))/(4 + Txx(y,x));
+            v(y,x) = (1-relax)*v(y,x) + relax * (sumV - (Txy(y,x).*u(y,x) + Tyt(y,x)))/(4 + Tyy(y,x));
+        end;
+%         sumU = u(y-1,xx) + u(y+1,xx) + u(y,xx-1) + u(y,xx+1);
+%         sumV = v(y-1,xx) + v(y+1,xx) + v(y,xx-1) + v(y,xx+1);
+%         u(y,xx) = (1-relax)*u(y,xx) + relax * (sumU - (Txy(y,xx).*v(y,xx) + Txt(y,xx)))/(4 + Txx(y,xx));
+%         v(y,xx) = (1-relax)*v(y,xx) + relax * (sumV - (Txy(y,xx).*u(y,xx) + Tyt(y,xx)))/(4 + Tyy(y,xx));
     end;
     
     % Von Neumann boundary conditions: set the perimeter values to the same
@@ -74,6 +85,13 @@ for i = 1:iterations
     display(sprintf('Step: %4d\t=> u:\t%f\t[%f\t%f]', i, mean(u(:)), min(u(:)), max(u(:))));
     display(sprintf('Step: %4d\t=> v:\t%f\t[%f\t%f]', i, mean(v(:)), min(v(:)), max(v(:))));
     
+    % Compute difference
+    diffU = sum(abs(u(:) - lastU(:)));
+    diffV = sum(abs(v(:) - lastV(:)));
+    display(sprintf('Step: %4d\t=> diff:\t[%f\t%f]', i, diffU, diffV));
+    lastU = u;
+    lastV = v;
+    
     % Compute error; use motion information to warp I2 onto I1
     img2warp = interp2(img2, xxg+u, yyg+v, 'linear', meanval);
     diff = (img1-img2warp).^2;
@@ -81,7 +99,8 @@ for i = 1:iterations
     dispimg(diffsm); pause(0.01);
     err(i) = sum(diffsm(:));
     display(sprintf('Step: %4d\t=> e:\t%f', i, err(end)));
-    if (err(end) > err(max(end-1,1))) break; end;
+    % if (err(end) > err(max(end-1,1))) break; end;
+    if (sum(diffU+diffV) < 0.000005 && err(end) > err(max(end-1,1))) break; end;
 end;
 
 % flow(:,:,1) = u;
