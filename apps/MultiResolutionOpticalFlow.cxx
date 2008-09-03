2beaa6de2de8ff3deec3f5be64818765124126d5
beastwoo <beastwoo>
2008-09-03 20:25:01 +0000

    Changed command line multires flow to use new pipeline structure.

--- apps/MultiResolutionOpticalFlow.cxx
+++ apps/MultiResolutionOpticalFlow.cxx
@@ -4,8 +4,9 @@
 
 #include "FilePattern.h"
 #include "FileSet.h"
+#include "ImageFileSetReader.h"
+#include "ImageFileSetTypes.h"
 #include "MultiResolutionOpticalFlowPipeline.h"
-#include "ImageSetReader.h"
 #include "Logger.h"
 #include "TextPipelineObserver.h"
 
@@ -19,7 +20,7 @@ int main(int argc, char** argv)
     Logger::debug << function << ": Parsing parameters" << std::endl;
     if (argc < 6)
     {
-        Logger::warning << "Usage:\n\t" << argv[0] << " dir formatIn start end formatOut [sigma] [weight] [iterations] [relaxation] [levels]" << std::endl;
+        Logger::warning << "Usage:\n\t" << argv[0] << " dir formatIn start end formatOut [sigmaD] [sigmaI] [regularization] [iterations] [levels]" << std::endl;
         return 1;
     }
 
@@ -28,27 +29,26 @@ int main(int argc, char** argv)
     int start = atoi(argv[3]);
     int end = atoi(argv[4]);
     std::string formatOut = argv[5];
-    float sigma = argc > 6 ? atof(argv[6]) : 1.0;
-    float weight = argc > 7 ? atof(argv[7]) : 10e4;
-    int iterations = argc > 8 ? atoi(argv[8]) : 200;
-    float relaxation = argc > 9 ? atof(argv[9]) : 1.9;
+    float sigmaI = argc > 6 ? atof(argv[6]) : 1.0;
+    float sigmaD = argc > 7 ? atof(argv[7]) : 2.4;
+    float regularization = argc > 8 ? atof(argv[8]) : 10e4;
+    int iterations = argc > 9 ? atoi(argv[9]) : 400;
     int levels = argc > 10 ? atoi(argv[10]) : 3;
     
     Logger::debug << function << ": Setting up image I/O" << std::endl;
-    typedef itk::Image< unsigned short, 2 > InputImageType;
-    typedef itk::Image< float, 2 > InternalImageType;
     FileSet filesIn(FilePattern(dir, formatIn, start, end));
     FileSet filesOut(FilePattern(dir, formatOut, start, end));
-    ImageSetReader<InputImageType, InternalImageType> video(filesIn);
+    ImageFileSetReader::Pointer images = ImageFileSetReader::New();
+    images->SetFileSet(filesIn);
     
     Logger::debug << function << ": Setting up optical flow pipeline" << std::endl;
     MultiResolutionOpticalFlowPipeline::Pointer pipeline = MultiResolutionOpticalFlowPipeline::New();
-    pipeline->SetInput(&video);
+    pipeline->SetInput(images);
     pipeline->SetOutputFiles(filesOut);
-    pipeline->SetSpatialSigma(sigma);
-    pipeline->SetRegularization(weight);
+    pipeline->SetSpatialSigma(sigmaD);
+    pipeline->SetIntegrationSigma(sigmaI);
+    pipeline->SetRegularization(regularization);
     pipeline->SetIterations(iterations);
-    pipeline->SetRelaxation(relaxation);
     pipeline->SetNumberOfLevels(levels);
     
     TextPipelineObserver::Pointer observer = TextPipelineObserver::New();
